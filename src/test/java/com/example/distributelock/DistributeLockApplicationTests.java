20c7ceda2f190d53bc80fdcf47074f899a64c46f
SIYUAN XU <xsiyuan@gmail.com>
2022-01-25 09:14:16 +0800

    redis 加锁解决集群 job 多发任务问题 zk 瞬时有序节点，解决集群 job 多发任务问题

---
+++ src/test/java/com/example/distributelock/DistributeLockApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example.distributelock;
+
+import com.example.distributelock.lock.ZkLock;
+import java.io.IOException;
+import org.apache.zookeeper.KeeperException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DistributeLockApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+
+  @Test
+  public void testZkLock() throws IOException {
+    ZkLock zkLock = new ZkLock("localhost:2181", "order");
+    try {
+      zkLock.getLock();
+      zkLock.close();
+    } catch (KeeperException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
